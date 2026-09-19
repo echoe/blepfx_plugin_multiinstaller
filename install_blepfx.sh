d4278302c35b246ef13f5b4906f9819bfe396cb7
Iris <3508085+echoe@users.noreply.github.com>
2026-09-19 16:59:17 -0500

    Update install_blepfx.sh
    bugfix and small cleanup

--- install_blepfx.sh
+++ install_blepfx.sh
@@ -1,47 +1,40 @@
 # tiny install script for blepfx plugins
-# current installs:
-# spectra : curl https://fx.amee.ee/spectra.sh | sh
-# prisma : curl https://fx.amee.ee/prisma.sh | sh
-# filtrr : curl https://fx.amee.ee/filtrr.sh | sh
-# crunchrr : curl https://fx.amee.ee/crunchrr.sh | sh
-# destruqtor : curl https://fx.amee.ee/destruqtor.sh | sh
+plugin_explainer="1:destruqtor | 2:crunchrr | 3:filtrr | 4:prisma | 5:spectra"
+
 if [[ "$1" == "sh" ]]; then
-  echo "this script does not support curl | sh install, please instead:"
-  echo "curl https://raw.githubusercontent.com/echoe/blepfx_plugin_multiinstaller/refs/heads/main/install_blepfx.sh -o install_blepfx.sh; sh install_blepfx.sh"
+  echo "this script does not support curl | sh install, please instead run it like this:"
+  echo "curl https://raw.githubusercontent.com/echoe/blepfx_plugin_multiinstaller/refs/heads/main/install_blepfx.sh -o install_blepfx.sh; sh install_blepfx.sh 12345 f"
+  exit
 fi
+
 if [[ "$1" == "help" ]]; then
   echo "welcome to the blepfx multi-installer for linux. made by echoe in about 20 minutes."
-  echo "install the same way the blepfx installs work. or if you've downloaded this ..."
-  echo "you can type in the number of the plugin you want and this will install them."
-  echo "for instance, running 'sh install_blepfx.sh 12345' will install all plugins."
+  echo "this installs any/all of the plugins at the same time, instead of one at a time."
+  echo "to install a plugin, type its number as part of the first string after the script."
+  echo $plugin_explainer
+  echo "running 'sh install_blepfx.sh 12345 f' will force install all plugins."
   exit;
 fi
+
 if [[ "$#" -eq 0 ]]; then
-  echo "no arguments supplied. type out the plugin numbers you want to have installed"
-  read -p "1: destruqtor \n 2: crunchrr \n 3:filtrr \n 4:prisma \n 5:spectra" selection
+  echo "no arguments supplied. type out the plugin numbers you want to have installed, and we'll install them."
+  read -p $plugin_explainer selection
 else
-  if "$1" == "sh"; then selection=$2; else selection=$1; fi
-  if [[ "$3" == "f" ]]; then
-    echo "installing plugins in $selection immediately";
-  else
-    echo "you are installing these plugin numbers: $selection"
-    read -p "type y to continue. type anything else to exit" continue
-    if [[ $continue == "y" ]]; then pass; else exit; fi
-  fi
+  selection=$1
 fi
-if [[ $selection == *"1"* ]]; then
-  curl https://fx.amee.ee/destruqtor.sh | sh
+
+if [[ "$2" == "f" ]]; then
+  echo "installing plugins in $selection immediately";
+else
+  echo "you are installing these plugin numbers: $selection"
+  read -p "type y to continue. type anything else to exit" continue
+  if [[ $continue == "y" ]]; then pass; else exit; fi
 fi
-if [[ $selection == *"2"* ]]; then
-  curl https://fx.amee.ee/crunchrr.sh | sh
-fi
-if [[ $selection == *"3"* ]]; then
-  curl https://fx.amee.ee/filtrr.sh | sh
-fi
-if [[ $selection == *"4"* ]]; then
-  curl https://fx.amee.ee/prisma.sh | sh
-fi
-if [[ $selection == *"5"* ]]; then
-  curl https://fx.amee.ee/spectra.sh | sh
-fi
+
+if [[ $selection == *"1"* ]]; then curl https://fx.amee.ee/destruqtor.sh | sh; fi
+if [[ $selection == *"2"* ]]; then curl https://fx.amee.ee/crunchrr.sh | sh; fi
+if [[ $selection == *"3"* ]]; then curl https://fx.amee.ee/filtrr.sh | sh; fi
+if [[ $selection == *"4"* ]]; then curl https://fx.amee.ee/prisma.sh | sh; fi
+if [[ $selection == *"5"* ]]; then curl https://fx.amee.ee/spectra.sh | sh; fi
+
 echo "installs finished for $selection"
